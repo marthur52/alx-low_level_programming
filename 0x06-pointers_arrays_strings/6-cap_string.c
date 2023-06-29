c90dfd843fdf6b03fff4e020f81321a0b2c7d417
marthur52 <michaelarthur018@gmail.com>
2023-06-29 13:45:26 +0000

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,6 +31,6 @@ char *cap_string(char *str)
 			index == 0)
 		str[index] -= 32;
 		index++;
-    }
-    return (str);
+	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+
  /**
   * leet - encodes a string into 1337speak
   * @n: input value
   *
-  * Return: n
+  * Return: pointer to the modified string
   */
+char *leet(char *n);
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char str[] = "Hello, World!";
+
+	printf("Original: %s\n", str);
+	printf("leet: %s\n", leet(str));
+	return (0);
+}
+
 char *leet(char *n)
 {
 	int m, p;
@@ -18,6 +36,7 @@ char *leet(char *n)
 			if (n[m] == s1[p])
 			{
 				n[m] = s2[p];
+				break;
 			}
 		}
 	}
